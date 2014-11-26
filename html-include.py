c6e028910e8620d255f4ad8ee7aceac433d99723
Bodecker DellaMaria <bojdell@gmail.com>
2014-11-26 12:34:18 -0600

    fixed formatting of html output

--- html-include.py
+++ html-include.py
@@ -3,7 +3,6 @@ import sys
 import re
 from os import listdir
 from os import path
-# from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 from bs4 import Comment
 
@@ -16,9 +15,10 @@ class Includer():
 		super().__init__()
 		self.input_dir = input_dir
 		self.output_dir = output_dir
-		self.input_files = []
+		self.shtml_files = []
 		self.inc_files = []
 
+		# set default indent to 4 spaces in BeautifulSoup.prettify()
 		orig_prettify = BeautifulSoup.prettify
 		r = re.compile(r'^(\s*)', re.MULTILINE)
 		def prettify(self, encoding=None, formatter="minimal", indent_width=4):
@@ -27,35 +27,42 @@ class Includer():
 
 	def processFiles(self):
 		"""Process all SHTML files in input_dir and write them as HTML files to output_dir"""
-		print("input_dir: " + self.input_dir)
-		print("output_dir: " + self.output_dir)
 
+		# iterate over all files in self.input_dir, collecting all .shtml and .inc files
 		for f in listdir(self.input_dir):
 			fName, fExt = path.splitext(f)
 			if fExt == ".shtml":
-				self.input_files.append(f)
+				self.shtml_files.append(f)
 			elif fExt == ".inc":
 				self.inc_files.append(f)
 
-		for f in self.input_files:
+		# process each .shtml file individually via self.__processFile()
+		for f in self.shtml_files:
 			fName = path.splitext(f)[0]
 			input_file = self.input_dir + '/' + f
 			output_file = self.output_dir + '/' + fName + ".html"
 			self.__processFile(input_file, output_file, False)
 
 	def __processFile(self, input_file, output_file, recursing):
-		"""Process a single SHTML file at path input_file and write processed HTML file to path output_file"""
+		"""Process a single file at path input_file and write processed file to path output_file.
+		May be called recursively depending on template structure.
+		"""
 		f_in = open(input_file, "r", encoding="utf-8")
-
 		soup = BeautifulSoup(f_in)
+
+		# find all comments in the document
 		comments = soup.findAll(text=lambda text:isinstance(text, Comment))
 
-		# parse out the .inc filepath from each comment, recursively obtain the content,
+		# parse out the template filepath from each comment, recursively obtain the content,
 		# and replace the comment with the content to be inserted
 		for comment in comments:
-			inc_path = re.match('^#include.+?virtual=\"(.+?)\"|^#include.+?file=\"(.+?)\"', comment).group(1)
-			inc_content = self.__processFile(self.input_dir + '/' + inc_path, "", True)
-			comment.replace_with(inc_content)
+			# TODO: fix regex to include 'file' keyword
+			inc_path = re.match('^#include.+?virtual=\"(.+?)\"', comment).group(1)
+			if inc_path:
+				inc_content = BeautifulSoup(self.__processFile(self.input_dir + '/' + inc_path, "", True))
+				for tag in inc_content.findAll(True):
+					comment.insert_before(tag)
+				comment.extract()
 		f_in.close()
 
 		# if we are recursing, return our result as a string so it can be included recursively
@@ -67,11 +74,13 @@ class Includer():
 			f_out = open(output_file, "w", encoding="utf-8")
 			f_out.write(soup.prettify(formatter=None))
 			f_out.close()
-		
 
 if __name__ == "__main__":
 	if len(sys.argv) < 3:
 		print("Usage: python3 html-include.py <input_dir> <output_dir>")
 	else:
 		includer = Includer(sys.argv[1], sys.argv[2])
+		print("Processing files in: " + sys.argv[1])
 		includer.processFiles()
+		print("Files processed successfully. Written to: " + sys.argv[2])
+
